10cc33a593e91c1ade1fa692abbe7826a949cab5
Royce Whitaker <royce@rdw.io>
2016-03-02 14:02:55 -0800

    Updated Talking

--- client/js/npc.js
+++ client/js/npc.js
@@ -15,65 +15,23 @@ define(['character'], function(Character) {
         ],
 
         "villager": [
-            "Howdy stranger. Do you like poetry?",
-            "Roses are red, violets are blue...",
-            "I like hunting rats, and so do you...",
-            "The rats are dead, now what to do?",
-            "To be honest, I have no clue.",
-            "Maybe the forest, could interest you...",
-            "or instead, cook a rat stew."
+             "lorem ipsum dolor sit amet",
+            "consectetur adipisicing elit, sed do eiusmod tempor"
         ],
 
         "agent": [
-            "Have you tried to bend that sword?",
-            "You must first connect to the inner sword..",
-            "and realize the truth...",
-            "There is no sword."
+             "lorem ipsum dolor sit amet",
+            "consectetur adipisicing elit, sed do eiusmod tempor"
         ],
 
         "rick": [
-            "We're no strangers to love",
-            "You know the rules and so do I",
-            "A full commitment's what I'm thinking of",
-            "You wouldn't get this from any other guy",
-            "I just wanna tell you how I'm feeling",
-            "Gotta make you understand",
-            "Never gonna give you up",
-            "Never gonna let you down",
-            "Never gonna run around and desert you",
-            "Never gonna make you cry",
-            "Never gonna say goodbye",
-            "Never gonna tell a lie and hurt you"
+             "lorem ipsum dolor sit amet",
+            "consectetur adipisicing elit, sed do eiusmod tempor"
         ],
 
         "scientist": [{
-			"text": [//default
-				"Greetings.",
-				"I am the inventor of these two potions.",
-				"The red one will replenish your health points...",
-				"The orange one will turn you into a firefox and make you invincible...",
-				"But it only lasts for a short while.",
-				"So make good use of it!",
-				"Now if you'll excuse me, I need to get back to my experiments..."
-			]},
-			{"condition": function(game){return (game.player.invincible);},
-			 "text": [
-				"Did you not listen to what I said?!!",
-				"the famous fire-potion only lasts a few seconds",
-				"You shouldn't be wasting them talking to me…"
-			]},
-			{"condition": function(game){return ((game.player.getSpriteName() == "firefox")
-											&& !(game.player.invincible));},
-			 "text": [
-				"Ha ha ha, *name*",
-				"All that glitters is not gold…",
-				"-sigh-",
-				"Did you really think you could abuse me with your disguise?",
-				"I conceived that f…, that potion.",
-				"Better not use your outfit as a deterrent,",
-				"The goons you'll meet will attack you whatever you look like."
-			]}
-			
+	    "lorem ipsum dolor sit amet",
+            "consectetur adipisicing elit, sed do eiusmod tempor"
 		],
 
         "nyan": [
@@ -104,57 +62,33 @@ define(['character'], function(Character) {
         ],
 
         "priest": [
-            "Oh, hello, young man.",
-            "Wisdom is everything, so I'll share a few guidelines with you.",
-            "You are free to go wherever you like in this world",
-            "but beware of the many foes that await you.",
-            "You can find many weapons and armors by killing enemies.",
-            "The tougher the enemy, the higher the potential rewards.",
-            "You can also unlock achievements by exploring and hunting.",
-            "Click on the small cup icon to see a list of all the achievements.",
-            "Please stay a while and enjoy the many surprises of BrowserQuest",
-            "Farewell, young friend."
+             "lorem ipsum dolor sit amet",
+            "consectetur adipisicing elit, sed do eiusmod tempor"
         ],
 
         "sorcerer": [
-            "Ah... I had foreseen you would come to see me.",
-            "Well? How do you like my new staff?",
-            "Pretty cool, eh?",
-            "Where did I get it, you ask?",
-            "I understand. It's easy to get envious.",
-            "I actually crafted it myself, using my mad wizard skills.",
-            "But let me tell you one thing...",
-            "There are lots of items in this game.",
-            "Some more powerful than others.",
-            "In order to find them, exploration is key.",
-            "Good luck."
+             "lorem ipsum dolor sit amet",
+            "consectetur adipisicing elit, sed do eiusmod tempor"
         ],
 
         "octocat": [
-            "Welcome to BrowserQuest!",
-            "Want to see the source code?",
-            'Check out <a target="_blank" href="http://github.com/browserquest/BrowserQuest">the repository on GitHub</a>'
+             "lorem ipsum dolor sit amet",
+            "consectetur adipisicing elit, sed do eiusmod tempor"
         ],
 
         "coder": [
-            "Hi! Do you know that you can also play BrowserQuest on your tablet or mobile?",
-            "That's the beauty of HTML5!",
-            "Give it a try..."
+             "lorem ipsum dolor sit amet",
+            "consectetur adipisicing elit, sed do eiusmod tempor"
         ],
 
         "beachnpc": [
-            "Don't mind me, I'm just here on vacation.",
-            "I have to say...",
-            "These giant crabs are somewhat annoying.",
-            "Could you please get rid of them for me?"
+             "lorem ipsum dolor sit amet",
+            "consectetur adipisicing elit, sed do eiusmod tempor"
         ],
 
         "desertnpc": [
-            "One does not simply walk into these mountains...",
-            "An ancient undead lord is said to dwell here.",
-            "Nobody knows exactly what he looks like...",
-            "...for none has lived to tell the tale.",
-            "It's not too late to turn around and go home, kid."
+             "lorem ipsum dolor sit amet",
+            "consectetur adipisicing elit, sed do eiusmod tempor"
         ],
 
         "othernpc": [
